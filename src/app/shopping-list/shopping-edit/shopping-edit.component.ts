fa613a781fc568926be9d26841805f70a4010a74
muradzalov <mzalov@gmail.com>
2021-07-19 18:45:27 -0500

    Adding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -18,7 +18,7 @@ export class ShoppingEditComponent implements OnInit {
 
   onAddItem() {
     const ingredientName = this.nameInputRef.nativeElement.value;
-    const ingredientAmount = this.nameInputRef.nativeElement.value;
+    const ingredientAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingredientName, ingredientAmount);
     this.ingredientAdded.emit(newIngredient);
   }
